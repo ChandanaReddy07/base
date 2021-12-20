cc9eb0d1e5500f10a310fd39347e374d0b766e36
dcr7978933k <b219021@iiit-bh.ac.in>
2021-12-20 14:30:02 +0530

    make it better

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var userRoutes = require('./routes/users');
 var cors = require('cors')
 
 //db connection
-mongoose.connect('mongodb+srv://chandana:54321@cluster0.hopev.mongodb.net/test',{
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
