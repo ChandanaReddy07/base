15384338e570273be4d6845799644c794702c7b2
dcr7978933k <b219021@iiit-bh.ac.in>
2021-12-20 16:01:30 +0530

    updated

---
+++ app.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const app = express();
+require('dotenv').config()
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var userRoutes = require('./routes/users');
+var cors = require('cors')
+
+//db connection
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  
+}).then(()=>{
+    console.log("DB CONNECTED")
+}).catch(
+    console.log("DB NOT CONNECTED")
+);
+
+const PORT = process.env.PORT || 8080;
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+//routes
+app.use("/",userRoutes);
+
+
+app.listen(PORT,()=>console.log(`haloo running in port${PORT}..`))
