e8c95d4a1f88bb749c4d97929cf8f774a3fe4554
dcr7978933k <b219021@iiit-bh.ac.in>
2021-12-20 12:11:07 +0530

    updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var userRoutes = require('./routes/users');
+var cors = require('cors')
 
 //db connection
 mongoose.connect(process.env.DATABASE,{
@@ -21,10 +22,9 @@ const PORT = process.env.PORT || 8080;
 //middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 //routes
 app.use("/api",userRoutes);
-app.use("/api",(req,res)=>(
-    res.json("hello there ..!")
-));
+
 
 app.listen(PORT,()=>console.log(`haloo running in port${PORT}..`))
